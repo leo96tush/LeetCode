c9f9c06b95de01c73a6e8e15437975d8ec613e40
leo96tush <41864567+leo96tush@users.noreply.github.com>
2020-06-20 14:29:43 +0530

    Create Permutation Sequence.cpp

---
+++ Permutation Sequence.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string getPermutation(int n, int k) {
+
+        string s ;
+
+        for ( int i = 1 ; i <= n ; i++ ) {
+            s = s + to_string(i) ;
+        }
+
+        for ( int i = 1 ; i < k ; i++ ) {
+            next_permutation(s.begin(), s.end()) ;
+        }
+
+        return s ;
+    }
+};
