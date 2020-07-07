1ef8f566a51bd8a2489d6483ad215ac7a1c1a93b
leo96tush <41864567+leo96tush@users.noreply.github.com>
2020-07-07 12:56:10 +0530

    Update Island Perimeter.cpp

--- Island Perimeter.cpp
+++ Island Perimeter.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+	// Checking for the cell existing on the grid
 	bool check(vector<vector<int>>& grid , int i , int j) {
 		int rows = grid.size() , cols = grid[0].size() ;
 		if (i >= 0 && i < rows && j >= 0 && j < cols) return true ;
@@ -14,6 +15,10 @@ public:
 		int rows = grid.size() , cols = grid[0].size() ;
 
 		int count = 0 ;
+
+		// Visiting each cell and adding perimeter
+		// if next cells from four sides is either zero
+		// or the next cell does not exist on the grid
 
 		for (int i = 0 ; i < rows ; i++) {
 			for (int j = 0 ; j < cols ; j++) {
