d6560811174678432f375a737132c3bf14fd3c3b
leo96tush <41864567+leo96tush@users.noreply.github.com>
2020-08-01 13:48:42 +0530

    Create Detect Capital.cpp

---
+++ Detect Capital.cpp
@@ -0,0 +1,41 @@
+/*
+Space Complexity : O(1)
+Time Complexity : O(n)
+*/
+
+class Solution {
+public:
+    bool allUpperCase(string word) {
+        for ( int i = 0 ; i < word.length() ; i++ ) {
+            if ( !(word[i] >= 'A' && word[i] <= 'Z') ) {
+                return false ;
+            }
+        }
+        return true ;
+    }
+
+    bool allLowerCase(string word) {
+        for ( int i = 0 ; i < word.length() ; i++ ) {
+            if ( !(word[i] >= 'a' && word[i] <= 'z') ) {
+                return false ;
+            }
+        }
+        return true ;
+    }
+
+    bool firstUpperCase(string word) {
+        for ( int i = 0 ; i < word.length() ; i++ ) {
+            if ( i == 0 && !(word[i] >= 'A' && word[i] <= 'Z') ) {
+                return false ;
+            }
+            if ( i != 0 && !(word[i] >= 'a' && word[i] <= 'z') ) {
+                return false ;
+            }
+        }
+        return true ;
+    }
+
+    bool detectCapitalUse(string word) {
+        return (allUpperCase(word) == true || allLowerCase(word) == true || firstUpperCase(word) == true) ;
+    }
+};
