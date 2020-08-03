835b7595c8de23402d118204ae636f8420a1816c
leo96tush <41864567+leo96tush@users.noreply.github.com>
2020-08-04 01:48:38 +0530

    Create Valid Palindrome.cpp

---
+++ Valid Palindrome.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string dummy = "" ;
+
+        for ( int i = 0 ; i < s.length() ; i++ ) {
+            if ( (s[i] >= 'a' && s[i] <= 'z') ) {
+                dummy += s[i] ;
+            }
+            else if ( (s[i] >= 'A' && s[i] <= 'Z') ) {
+                dummy += tolower(s[i]) ;
+            }
+            else if ( (s[i] >= '0' && s[i] <= '9') ) {
+                dummy += s[i] ;
+            }
+        }
+
+        int i = 0 , j = dummy.length() - 1 ;
+
+        while ( i <= j ) {
+            if ( dummy[i] != dummy[j] ) {
+                cout << i << endl ;
+                return false ;
+            }
+            i++ ; j-- ;
+        }
+        return true ;
+    }
+};
