7959944f858442d873181593824b44630fd3cba6
delhivery <tushar.gupta5@delhivery.com>
2024-03-30 10:29:32 +0530

    Number of Arithmetic Triplets Single Loop

--- Number of Arithmetic Triplets.py
+++ Number of Arithmetic Triplets.py
@@ -8,3 +8,35 @@ class Solution:
                     if (nums[j] - nums[i] == diff) and (nums[k] - nums[j] == diff):
                         count += 1
         return count
+
+# Single Loop Solution
+class Solution:
+    def arithmeticTriplets(self, nums: List[int], diff: int) -> int:
+        count = 0
+        i = 0
+        j = 1
+        k = 2
+
+        while(i < j and j < k and k<= len(nums)-1):
+            if (nums[k] - nums[j] == diff) and (nums[j] - nums[i] == diff):
+                count += 1
+                k += 1
+                continue
+            elif nums[k] - nums[j] > diff and k > j+1:
+                j += 1
+                continue
+            elif nums[k] - nums[j] < diff:
+                k += 1
+                continue
+            elif nums[j] - nums[i] > diff and j > i+1:
+                i += 1
+                continue
+            elif nums[j] - nums[i] < diff and k > j+1:
+                j += 1
+                continue
+            elif nums[j] - nums[i] < diff and k == j+1:
+                j += 1
+                k += 1
+                continue
+            k += 1
+        return count 
