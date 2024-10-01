5f47cfa61d580cb5bdf078c11739d7f4178ba7af
delhivery <tushar.gupta5@delhivery.com>
2024-10-01 22:06:53 +0530

    duplicate emails

--- Duplicate Emails.sql
+++ Duplicate Emails.sql
@@ -1,6 +1,5 @@
 # Write your MySQL query statement below
 
-
 WITH DATA AS (
     SELECT Person.email AS Email, count(Person.email) AS email_count 
     FROM Person
@@ -10,3 +9,13 @@ WITH DATA AS (
 SELECT DATA.Email as Email
 FROM DATA
 WHERE DATA.email_count > 1 ;
+
+
+
+-- Write your PostgreSQL query statement below
+SELECT email FROM (
+    SELECT email, COUNT(email) AS email_count
+    FROM Person
+    GROUP BY email
+)
+WHERE email_count > 1
