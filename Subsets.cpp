53144e65df7218722dbd59cb8f33d21e6484de3c
delhivery <tushar.gupta5@delhivery.com>
2024-10-20 20:33:26 +0530

    Refactor subset generation using binary representation

--- Subsets.cpp
+++ Subsets.cpp
@@ -1,6 +1,5 @@
 class Solution {
 public:
-
     /**
      * Generates all possible subsets of a given set of integers.
      *
@@ -43,6 +42,87 @@ public:
         // Return the complete list of subsets
         return output;
     }
-
-    }
 };
+
+/**
+ * @class Solution
+ * @brief Provides a solution to generate all possible subsets of a given set of integers.
+ */
+
+/**
+ * @brief Converts a given number to its binary representation as a string.
+ * 
+ * This function converts a given integer to its binary representation and 
+ * ensures the binary string has a fixed length by padding with zeros.
+ * 
+ * @param number The integer to be converted to binary.
+ * @param number_size The fixed length of the binary string.
+ * @return A string representing the binary form of the given number.
+ */
+string bool_string(int number, int number_size) {
+    // Initialize an empty string to store the binary representation
+    string out = "";
+
+    // Convert the number to binary
+    while(number > 0) {
+        // Append the remainder of number divided by 2 (0 or 1) to the string
+        out += to_string(number % 2);
+        // Divide the number by 2
+        number = number / 2;
+    }
+
+    // Calculate the number of zeros needed to pad the binary string to the fixed length
+    number_size = number_size - out.size();
+
+    // Pad the binary string with zeros
+    while(number_size > 0) {
+        out += "0";
+        number_size--;
+    }
+
+    // Return the binary string
+    return out;
+}
+
+/**
+ * @brief Generates all possible subsets of a given set of integers.
+ * 
+ * This function generates all subsets of the input vector `nums` using 
+ * binary representation. Each subset corresponds to a binary number where 
+ * each bit represents whether an element is included in the subset.
+ * 
+ * @param nums A vector of integers for which subsets are to be generated.
+ * @return A vector of vectors, where each inner vector is a subset of `nums`.
+ */
+vector<vector<int>> subsets(vector<int>& nums) {
+    // Initialize the output vector to store all subsets
+    vector<vector<int>> output;
+
+    // Calculate the total number of subsets (2^n)
+    int number = pow(2, nums.size());
+    // Get the size of the input vector
+    int nums_size = nums.size();
+
+    // Iterate over each possible subset (from 0 to 2^n - 1)
+    for(int i = 0; i < number; i++) {
+        // Initialize a temporary vector to store the current subset
+        vector<int> temp;
+
+        // Get the binary representation of the current number
+        string curr_string = bool_string(i, nums_size);
+
+        // Iterate over each bit in the binary string
+        for(int j = 0; j < curr_string.size(); j++) {
+            // If the bit is '1', include the corresponding element in the subset
+            if(curr_string[j] == '1') {
+                temp.push_back(nums[j]);
+            }
+        }
+
+        // Add the current subset to the output vector
+        output.push_back(temp);
+    }
+
+    // Return the complete list of subsets
+    return output;
+}
