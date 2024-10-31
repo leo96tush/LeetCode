92d7f6fa0187d86931953e74ef524fe21b6f7c33
delhivery <tushar.gupta5@delhivery.com>
2024-10-31 16:49:25 +0530

    Longest Substring Without Repeating Characters.cpp

--- Longest Substring Without Repeating Characters.cpp
+++ Longest Substring Without Repeating Characters.cpp
@@ -1,20 +1,33 @@
 class Solution {
 public:
 	int lengthOfLongestSubstring(string s) {
-		int n = int(s.length()) ;
-		int output = 0 ;
+		// Initialize starting index of the current substring (i),
+		// the length of the longest substring found so far (output),
+		// and the size of the input string (s_size).
+		int i = 0;
+		int output = 0;
+		int s_size = s.size();
 
+		// Create an unordered map to store the last seen position of each character.
 		unordered_map<char, int> mp;
 
-		for( int i=0, j=0 ; j<n ; j++){
-			if(mp[s[j]] > 0){
-				i = max(mp[s[j]], i) ;
+		// Iterate over each character in the string with j as the ending index of the current substring.
+		for(int j = 0; j < s_size; j++) {
+
+			// If the character at index j has been seen before and its last seen position is
+			// within the current window, update i to exclude the previous occurrence of s[j].
+			if(mp[s[j]] > 0) {
+				i = max(mp[s[j]], i);
 			}
 
-			output = max(output, j-i+1) ;
-			mp[s[j]] = j+1 ;
+			// Calculate the current length of the substring and update output if it's the longest so far.
+			output = max(output, j - i + 1);
+
+			// Update the last seen position of the character s[j] to j + 1.
+			mp[s[j]] = j + 1;
 		}
 
-		return output ;
+		// Return the length of the longest substring without repeating characters.
+		return output;
 	}
 };
